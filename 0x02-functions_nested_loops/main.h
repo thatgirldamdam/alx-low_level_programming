e1d65146c79f9a1df50776ed007520193473bc35
Oladele Oluwadamilola <129033258+thatgirldamdam@users.noreply.github.com>
2023-10-10 16:50:12 +0000

    Updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,13 @@
 main.h
-void print_alphabet(void);
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
